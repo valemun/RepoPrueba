d29fa7f034480eda516c729c4ef17c4e631ef7ce
MaFerGlezG <16012603@udec.edu.mx>
2021-05-18 21:14:07 -0500

    avance

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log('Hola Valeria gracias por explicarme todo tqm')
+console.log('Hola Valeria gracias por explicarme todo tqm');
+console.log('<3');
+console.log('Imprimiendo por consola mientras trabajamos en sincronía')
