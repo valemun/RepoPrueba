9b82dc486e213a98069c1f04a21242bdd949db9e
valemun <valeria.munoz@hotmail.com>
2021-05-18 19:09:58 -0700

    funcion sumar

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hola Valeria gracias por explicarme todo tqm')
+console.log( 'Hola Valeria gracias por explicarme todo tqm' );
+
+let num1 = 3;
+let num2 = 5;
+
+function suma (num1, num2){
+    return num1 + num2;
+}
+
+console.log( suma(num1, num2) );
